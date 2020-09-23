366ca283536ac7b8e64a97e6e026dcbe8d6991bf
shashankkatte <shashankanand.katte@gmail.com>
2020-09-23 16:49:47 -0400

    src : add express router
    Refactor code from app.js to routes to use express router

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/add-product', (req, res, next) => {
-  console.log('In the second middleware!');
-  res.send(
-    '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  console.log('In the second middleware!');
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
