7ca2959ce6a8beb640fa341d4456dec2cf5cb35b
shashankkatte <shashankanand.katte@gmail.com>
2020-09-23 10:19:45 -0400

    refactor : add code comments

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //  Lets look at how easy it is to start a node server with express.js 
 const http = require('http');
 
+//  This is one new step with express hwere you create an instance of express
 const express = require('express');
-
 const app = express();
 
+// You can pass app as request handler
 const server = http.createServer(app);
 
 server.listen(3000);
