efbbe2748c53a4021adf92626d52eccc8766f648
shashankkatte <shashankanand.katte@gmail.com>
2020-09-23 12:15:15 -0400

    src : Parsing request Body
    Added third party npm package body-parser to parse request body

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In the second middleware!');
-    res.send('<h1>Add product page</h1>');
-  });
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/',(req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In the second middleware!');
+  res.send(
+    '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In the second middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
