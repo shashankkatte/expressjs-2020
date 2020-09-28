e11d40f3e8b1db93376e81fd367d2ee8709cbfd0
shashankkatte <shashankanand.katte@gmail.com>
2020-09-28 16:45:00 -0400

    src : Switching views to ejs
    Swithing templating engine for going forward to ejs for the project

--- app.js
+++ app.js
@@ -18,10 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    path: null,
-  });
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
