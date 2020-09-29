0473a609feaafbc20d68c5c03cd26b42eada792c
shashankkatte <shashankanand.katte@gmail.com>
2020-09-29 09:55:59 -0400

    src : reading and saving data to file
    Saving products data to file and accessing it through models

--- models/product.js
+++ models/product.js
@@ -1,13 +1,36 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class product {
   constructor(title) {
     this.title = title;
   }
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
